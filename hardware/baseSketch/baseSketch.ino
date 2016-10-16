827699092945880685faac59ce09842a1b1fd962
Greg Harper <Greg Harper>
2016-10-15 19:42:35 -0500

    Fixes pIR bullshit

--- hardware/baseSketch/baseSketch.ino
+++ hardware/baseSketch/baseSketch.ino
@@ -3,6 +3,9 @@
 #include <RCSwitch.h>
 #include <idDHT11.h>
 
+#define WAIT_TIME 4
+int loopstate = 0;
+
 // setup for DHT11
 #define DHT_PIN 2
 #define DHT_ISR_NUM 0
@@ -10,13 +13,11 @@ void dht11_wrapper();
 idDHT11 DHT11(DHT_PIN, DHT_ISR_NUM,dht11_wrapper);
 
 // PIR setup
-#define PIR_PIN 3
-#define PIR_CAL_TIME 30
-volatile int PIRposs;
-int PIRstatus = HIGH;
-volatile boolean  PIRchanged = false;
-volatile long unsigned int PIRchangeStamp = 0;
-const long unsigned int delayNoise = 1000;
+#define PIR_PIN 4
+#define PIR_CAL_TIME 60
+#define MAXCHECKS 100
+unsigned char state[MAXCHECKS]={0};    // Array that maintains bounce status
+char Index = 0;   
 
 //setup photoresistor
 #define PHOTO_PIN0 0
@@ -37,15 +38,13 @@ void setup()
   
   //Calibrate PIR sensor
   pinMode(PIR_PIN, INPUT);
-  digitalWrite(PIR_PIN, HIGH);
   Serial.print("Calibrating Motion Sensor");
-  for(int i = 0; i < PIR_CAL_TIME; i++)
+  for(int i = 0; i < PIR_CAL_TIME; i += 1)
   {
     Serial.print(".");
     delay(1000);
   }
   Serial.println("done.");
-  attachInterrupt(digitalPinToInterrupt(PIR_PIN), PIR_ISR, CHANGE);
   
   //Setup RC transmitter
   mySwitch.enableTransmit(10);
@@ -56,11 +55,10 @@ void setup()
 
 void loop()
 {
-  int loopstate, wait_time = 4; //In seconds
-  if(loopstate >= wait_time)
+  if(loopstate == WAIT_TIME*100)
   {
     String resp = "";
-    resp = resp + "PIR:" + isMovementPIR();
+    resp = resp + "PIR:" + debouncePIR();
     resp = resp + checkTemp();
     resp = resp + "photo:" + String(AVERAGE(analogRead(PHOTO_PIN0), analogRead(PHOTO_PIN1))) + ";";
     resp = resp + '\n';
@@ -70,8 +68,12 @@ void loop()
     }
     loopstate = 0;
   }
-  loopstate ++;
-  delay(1000);
+  else
+  {
+    debouncePIR();
+  }
+  loopstate +=1;
+  delay(10);
 }
 
 void serialEvent() 
@@ -89,34 +91,6 @@ void dht11_wrapper()
   DHT11.isrCallback();
 }
 
-void PIR_ISR()
-{
-  int readPIR = digitalRead(PIR_PIN);
-  if (PIRstatus != readPIR)
-  {                       
-    PIRchanged = true;
-    PIRposs = readPIR;
-    PIRchangeStamp = millis();
-  }
-  else
-  { 
-    PIRchanged = false;
-  }
-}
-
-String isMovementPIR()
-{
-  if (PIRchanged == true)
-  {
-    if ((millis() - PIRchangeStamp) > delayNoise)
-    {
-      PIRstatus = PIRposs ;
-      PIRchanged = false;
-    }
-  }
-  return String(PIRstatus == LOW);                                    
-}
-
 String checkTemp()
 {
   int chk = DHT11.acquireAndWait();
@@ -125,7 +99,6 @@ String checkTemp()
   {
      resp = resp + "temp:" + String(int(DHT11.getFahrenheit())) + ";humid:" + String(int(DHT11.getHumidity()))+ ";";
   }
-
   return resp;
 }
 
@@ -143,7 +116,7 @@ void sendIR(String data){
         unsigned long color = strtol(serData.c_str(), NULL, 16);
         Serial.println("IR Blasting: "+serData+"; "+color);
         int i;
-        for(i = 0;i<retries;i++)
+        for(i = 0;i<retries;i+=1)
         {
           irsend.sendNEC(color, 32);
           delay(100);
@@ -159,4 +132,16 @@ void sendIR(String data){
       }
       data = data.substring(data.indexOf(';')+1);
     }
+}
+
+int debouncePIR(void)
+{
+  state[Index] = (digitalRead(PIR_PIN) == HIGH) ? 1 : 0;
+  Index = (Index+1) % MAXCHECKS;
+  int i;
+  for (i = 0; i < MAXCHECKS; i++)
+  {
+    if(!state[i]) {return 0;}
+  }
+  return 1;
 }
