15d933fa9321f5f5da2702cc07b19b64392adbb6
Jordan Poles <jpoles1@gmail.com>
2016-10-27 11:41:41 -0500

    Added voice interpretation.

--- logic/dakAPI.js
+++ logic/dakAPI.js
@@ -1,22 +1,54 @@
 var dakAPI = {};
-//TODO: Create logic that accepts some sort of identifier for each command via a URL and runs the command
 dakAPI.generateRoutes = function(){
   api_router = express.Router();
-  api_router.get("/:actuator-:cmd", (req, res) => {
-    db.config.find({type: "actuator", $or: [{api_name: req.params.actuator}, {name: req.params.actuator}], active: 1})
+  handleCommand = function(actuator, cmd, cb){
+    db.config.find({type: "actuator", $or: [{api_name: actuator}, {name: actuator}], active: 1})
     .sort({_id: 1}).exec(function(err, docs){
       if(docs.length > 0){
         actuator = docs[0]
-        db.config.find({type: "actuator_command", actuator: actuator._id, $or: [{api_name: req.params.cmd}, {name: req.params.cmd}], active: 1})
+        db.config.find({type: "actuator_command", actuator: actuator._id, $or: [{api_name: cmd}, {name: cmd}], active: 1})
         .sort({_id: 1}).exec(function(err, docs){
           if(docs.length > 0){
             cmd = docs[0]
             dakActuators.sendActuatorCommand(cmd, () => {})
+            cb()
+          }
+          else{
+            cb("No such command found.  Try using all-lowercase with no spaces.")
           }
         })
       }
+      else{
+        cb("No actuator found with this name. Try using all-lowercase with no spaces.")
+      }
+    })
+  }
+  api_router.get("/:actuator-:cmd", (req, res) => {
+    actuator = req.params.actuator.replace(/\W/g, '').toLowerCase()
+    cmd = req.params.cmd.replace(/\W/g, '').toLowerCase()
+    handleCommand(actuator, cmd, (err) => {
+      if(err){
+        req.params.err = err
+      }
       res.json(req.params)
     })
+  })
+  api_router.get("/voice", (req, res) => {
+    if(req.query.speech && req.query.speech.split(" ").length>=2){
+      words = req.query.speech.split(" ")
+      actuator = words[0].replace(/\W/g, '').toLowerCase()
+      cmd = words[1].replace(/\W/g, '').toLowerCase()
+      handleCommand(actuator, cmd, (err) => {
+        req.params = {actuator, cmd}
+        if(err){
+          req.params.err = err
+        }
+        res.json(req.params)
+      })
+    }
+    else{
+      res.send("Failed to parse speech input")
+    }
   })
   router.use("/api", api_router);
 }
