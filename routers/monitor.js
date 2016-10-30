cfccd181de72a545d34b6f66cfc0ce6d237b19d7
Jordan Poles <jpoles1@gmail.com>
2016-10-30 11:49:52 -0500

    Adding monitor graphs.

--- routers/monitor.js
+++ routers/monitor.js
@@ -1,7 +1,7 @@
 router.get("/", (req, res) => {
-  db.activity.find({type: "sensorlog"}).sort({_id: -1}).limit(10000).exec((err, docs) => {
+  db.activity.find({type: "sensorlog"}).sort({time: 1}).limit(5000).exec((err, docs) => {
     res.page_data.roomdata = JSON.stringify(docs)
-    console.log(res.page_data)
+    //console.log(res.page_data)
     res.render("home.hbs", res.page_data)
   })
 })
